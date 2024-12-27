845036e784f5b0da98febe7ca18765b55e181a69
Vijaya Sri Ram <vijayasriram8@gmail.com>
2024-12-26 23:06:29 -0500

    Create Readme & Add 1st Py File

---
+++ Introduction/SayHelloWorldWithPython.py
@@ -0,0 +1,13 @@
+'''
+Question Link : https://www.hackerrank.com/challenges/py-hello-world/problem
+Question      : Say "Hello, World!" With Python
+Difficulty    : Easy
+Skills        : -
+Sub Domain    : Introduction
+Language Used : Python 3
+'''
+
+print("Hello, World!")
+
+#Time Complexity = O(1)
+#Space Complexity = O(1)
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # HackerRank_Python_Solutions
+
+- My personal version of solutions for the HackerRank Python Domain.
+
+<hr>
+
+- Introduction
+    - [Say "Hello, World!" With Python](Introduction/SayHelloWorldWithPython.py)
